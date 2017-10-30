0e301f02c00b89ea71e96b4aebbe2867d3c5bb5b
DaVinci Design Studio <33228525+davincidesignstudio@users.noreply.github.com>
2017-10-30 23:27:59 +0530

    Create hit-counter.php

---
+++ hit-counter.php
@@ -0,0 +1,25 @@
+<?php
+
+function HitCounter()
+{
+ 
+$TextFile = fopen("visitor-hit-counter.txt", "r");
+ 
+ $HitValue = (int) fread ($TextFile,20) ;
+ 
+ fclose ($TextFile) ;
+ 
+ $HitValue++ ;
+ 
+ echo $HitValue ;
+ 
+$TextFile = fopen("visitor-hit-counter.txt", "w" ) ;
+
+fwrite($TextFile,$HitValue) ;
+
+fclose ($TextFile) ;
+
+ }
+
+
+?>
